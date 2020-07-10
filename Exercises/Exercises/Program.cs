43423668551bbf32771e529ed0ee21e1690e9064
rhagen87 <reynold.hagen@gmail.com>
2020-07-10 13:24:49 -0500

    complete

---
+++ Exercises/Exercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            double length;
+            double width;
+            string input;
+
+            Console.WriteLine("Please enter the length of the rectangle:");
+            input = Console.ReadLine();
+            length = double.Parse(input);
+            Console.WriteLine("Please enter the width of the rectangle:");
+            input = Console.ReadLine();
+            width = double.Parse(input);
+            double area = length * width;
+            Console.WriteLine("The area of the rectangle is: " + area);
+        }
+    }
+}
